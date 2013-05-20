d62a6bef938932a32b3a271a8e47ee29f4bd17b2
JGroenestein <j.groenestein@gmail.com>
2013-05-21 04:21:32 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AceAdminTest::Application.config.secret_token = 'd9b075af8daf630c2dfee814e500223b004627dbbf9242c6c8e4727fc81b382588d5c8d2a1a05a8e388d87c7826189443dbfaacc22be8c785f20b29d8bf28010'
